93291c43c2e4ead5ac0e1da8d57be509d02645a6
luting <1364139550@qq.com>
2022-05-19 10:36:26 +0800

    主页头像渲染完毕

--- assets/js/baseUrl.js
+++ assets/js/baseUrl.js
@@ -1,6 +1,20 @@
 
 // 这个文件只是用来处理 基地址的
-$.ajaxPrefilter(function(options){
+$.ajaxPrefilter(function (options) {
     const baseUrl = 'http://www.liulongbin.top:3008'
     options.url = baseUrl + options.url
+    // 给url里面带 /my/ 字符串的加一个headers属性
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 全局挂载检查是否有 token令牌  的指令
+    options.complete = function (res) {
+        console.log(res)
+        if (res.responseJSON.code === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = './login.html'
+        }
+    }
 })
